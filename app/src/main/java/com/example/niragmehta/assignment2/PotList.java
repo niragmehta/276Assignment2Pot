e776a5ed3c8d6f3f06590e2d68b8a100a6b6f08a
niragmehta <nsmehta@sfu.ca>
2018-01-25 18:24:14 -0800

    Added List view and array adapter

--- app/src/main/java/com/example/niragmehta/assignment2/PotList.java
+++ app/src/main/java/com/example/niragmehta/assignment2/PotList.java
@@ -2,6 +2,8 @@ package com.example.niragmehta.assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PotList extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class PotList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pot_list);
+
+        PotCollection potCollection=new PotCollection();
+
+        ArrayAdapter<Pot> adapter=new ArrayAdapter<Pot>(
+                this,
+                R.layout.activity_pot_list,
+                potCollection.getPots()
+        );
+
+        ListView listView=findViewById(R.id.listViewListPots);
+        listView.setAdapter(adapter);
+
     }
+
+
 }
