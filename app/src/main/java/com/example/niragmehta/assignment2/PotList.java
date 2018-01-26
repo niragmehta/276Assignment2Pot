55fb4dbd75006bd429d0e22ca6edad78d3ff495f
Warren Ho Kin <warrenhokin@gmail.com>
2018-01-25 20:10:57 -0800

    Completed arrayAdapter and added listView XML.

--- app/src/main/java/com/example/niragmehta/assignment2/PotList.java
+++ app/src/main/java/com/example/niragmehta/assignment2/PotList.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,19 +20,6 @@ public class PotList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pot_list);
 
-        PotCollection potCollection=new PotCollection();
-
-
-
-        ArrayAdapter<Pot> adapter=new ArrayAdapter<Pot>(
-                this,
-                R.layout.activity_pot_list,
-                potCollection.getPots()
-        );
-
-        ListView listView=findViewById(R.id.listViewListPots);
-        listView.setAdapter(adapter);
-
         Button button=findViewById(R.id.btnAddPot);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -40,8 +29,45 @@ public class PotList extends AppCompatActivity {
             }
         });
 
-
+        pupulateListView();
+        registerOnClickCallback();
     }
 
+    private void pupulateListView() {
+        // Create a list of item
+        // Instantiate a data collection object
+        PotCollection potCollection = new PotCollection();
 
+
+        // ===============
+        potCollection.addPot(new Pot("Super bowl", 999));
+        potCollection.addPot(new Pot("My mini bowl", 1));
+
+        //================
+
+
+        // Build adaptor
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.pots,
+                potCollection.getPotDescriptions());
+
+        // Configure list view
+        ListView listView = (ListView) findViewById(R.id.listViewListPots);
+        listView.setAdapter(adapter);
+    }
+
+    private void registerOnClickCallback() {
+        final ListView listView = (ListView) findViewById(R.id.listViewListPots);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long is) {
+                TextView textView = (TextView) viewClicked;
+                String message = "Pot #" + position
+                        + " " + textView.getText().toString();
+            }
+        });
+
+    }
 }
