a586897513cd771226a24bd958f9cc999b615d06
niragmehta <nsmehta@sfu.ca>
2018-01-25 19:40:03 -0800

    added the third and final activity+layout

--- app/src/main/java/com/example/niragmehta/assignment2/CalculateServing.java
+++ app/src/main/java/com/example/niragmehta/assignment2/CalculateServing.java
@@ -2,6 +2,7 @@ package com.example.niragmehta.assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CalculateServing extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class CalculateServing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate_serving);
+
+
+        Button button=findViewById(R.id.btnBack);
+    }
+
+    public void back()
+    {
+
     }
 }
