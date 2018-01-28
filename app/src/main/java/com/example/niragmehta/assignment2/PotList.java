d08fb9718e5f7299f174c79c595b292f69d3c5e8
niragmehta <nsmehta@sfu.ca>
2018-01-27 21:42:14 -0800

    implemented edit Pot functionality

--- app/src/main/java/com/example/niragmehta/assignment2/PotList.java
+++ app/src/main/java/com/example/niragmehta/assignment2/PotList.java
@@ -1,11 +1,9 @@
 package com.example.niragmehta.assignment2;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,8 +17,9 @@ public class PotList extends AppCompatActivity {
 
     public static final int REQUEST_CODE_GET_POT = 2407;
     public static final int REQUEST_CODE_CALCULATE = 1321;
+    public static final int REQUEST_CODE_EDIT = 6969;
     public PotCollection potCollection = new PotCollection();
-
+    public int indexOfPot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,18 +46,18 @@ public class PotList extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch(requestCode) {
+        switch (requestCode) {
             case REQUEST_CODE_GET_POT:
-                if(resultCode == Activity.RESULT_OK){
+                if (resultCode == Activity.RESULT_OK) {
                     String potName = data.getStringExtra("The pot name");
-                    int potWeight = data.getIntExtra("The pot weight",0);
+                    int potWeight = data.getIntExtra("The pot weight", 0);
 
                     Pot pot = new Pot(potName, potWeight);
                     potCollection.addPot(pot);
                     populateListView();
 
                     Toast.makeText(getApplicationContext(),
-                             pot.getName() + " successfully added!",
+                            pot.getName() + " successfully added!",
                             Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(getApplicationContext(),
@@ -67,19 +66,30 @@ public class PotList extends AppCompatActivity {
                 }
                 break;
             case REQUEST_CODE_CALCULATE:
-                int potIndex = data.getIntExtra("The pot's index", -1);
+                if (resultCode == Activity.RESULT_OK) {
+                    int potIndex = data.getIntExtra("The pot's index", -1);
 
-                Log.i("debug", "The pot's index is " + potIndex);
-                if(potIndex == -1){
-                    Toast.makeText(getApplicationContext()," Pot index not found (?)",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    potCollection.removePot(potIndex);
-                    populateListView();
+                    Log.i("debug", "The pot's index is " + potIndex);
+                    if (potIndex == -1) {
+                        Toast.makeText(getApplicationContext(), " Pot index not found (?)",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        potCollection.removePot(potIndex);
+                        populateListView();
+                    }
                 }
 
+                break;
+            case REQUEST_CODE_EDIT:
+                if (resultCode == Activity.RESULT_OK) {
+                    String potName = data.getStringExtra("The pot name");
+                    int potWeight = data.getIntExtra("The pot weight", 0);
 
+                    Pot pot = new Pot(potName, potWeight);
+                    potCollection.changePot(pot, indexOfPot);
+                    populateListView();
 
+                }
         }
     }
 
@@ -101,18 +111,30 @@ public class PotList extends AppCompatActivity {
 
     private void registerOnClickCallback() {
         ListView list = (ListView) findViewById(R.id.listViewListPots);
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View viewClicked,
+                                           int position, long id) {
+                Intent intent = AddPot.makeIntent(PotList.this);
+
+                startActivityForResult(intent, REQUEST_CODE_EDIT);
+
+                return false;
+            }
+        });
+
+
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
                 TextView textView = (TextView) viewClicked;
 
-                String message = "Using #" + position + " " + textView.getText().toString();
-                Toast.makeText(PotList.this, message, Toast.LENGTH_LONG).show();
-
                 Intent intent = CalculateServing.makeIntent(PotList.this,
                         potCollection.getPot(position).getName(),
                         potCollection.getPot(position).getWeightInG(),
                         position);
+                indexOfPot = position;
 
                 startActivityForResult(intent, REQUEST_CODE_CALCULATE);
             }
